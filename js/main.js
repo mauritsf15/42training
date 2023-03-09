761fa0397632773552460094719810b0a41facd2
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-03-09 16:02:13 +0100

    Update clickable

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const container = document.querySelector('.container')
 container.addEventListener('click', function(data) {
     if (data.target.parentElement.classList[0] == 'intro' || data.target.classList[0] == 'intro') {
         window.location.href = "#focus";
-    } else if (data.target.parentElement.classList[0] == 'on-focus' || data.target.classList[0] == 'on-focus') {
+    } else if (
+        data.target.parentElement.classList[0] == 'inline' 
+        || data.target.parentElement.classList[0] == 'on-focus' 
+        || data.target.classList[0] == 'on-focus'
+        ) {
         //
     } else {
         window.location.href = "#";
