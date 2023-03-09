52489504bf700309c709274f6c71cf90cdf7545a
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-03-09 17:45:09 +0100

    Slideshow added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,48 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+
+let state = 'outFocus';
 
 container.addEventListener('click', function(data) {
+    
     if (data.target.parentElement.classList[0] == 'intro' || data.target.classList[0] == 'intro') {
         window.location.href = "#focus";
+        state = 'inFocus'
     } else if (
         data.target.parentElement.classList[0] == 'inline' 
         || data.target.parentElement.classList[0] == 'on-focus' 
         || data.target.classList[0] == 'on-focus'
         ) {
-        //
+            //
     } else {
         window.location.href = "#";
+        setTimeout(function() {
+            state = 'outFocus'
+        }, 50)
+    }
+    if (state == 'outFocus') {
+        changeImage();
     }
 })
+
+const image = document.querySelector('.container-image');
+const imageArray = ['bg-ph-1.jpg', 'bg-ph-2.jpg'];
+let currentImage = 0;
+let changingDia = false;
+
+function changeImage() {
+    currentImage++;
+    if (currentImage == imageArray.length) {
+        currentImage = 0;
+    }
+    if (changingDia == false) {
+        changingDia = true;
+        image.style.opacity = '0';
+        setTimeout(function() {
+            image.style.backgroundImage = `url(../img/${imageArray[currentImage]})`;
+            image.style.opacity = '0.5';
+        }, 1000);
+        setTimeout(function() {
+            changingDia = false;
+        }, 2000);
+    }
+}
