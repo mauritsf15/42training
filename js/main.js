bf2ffb7962e89df65d302a15e6514bfb4a1fa887
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-03-20 11:09:55 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const container = document.querySelector('.container');
 let state = 'outFocus';
 
 container.addEventListener('click', function(data) {
-    
     if (data.target.parentElement.classList[0] == 'intro' || data.target.classList[0] == 'intro') {
         window.location.href = "#focus";
         state = 'inFocus'
@@ -11,6 +10,9 @@ container.addEventListener('click', function(data) {
         data.target.parentElement.classList[0] == 'inline' 
         || data.target.parentElement.classList[0] == 'on-focus' 
         || data.target.classList[0] == 'on-focus'
+        || data.target.parentElement.classList[0] == 'left-side' 
+        || data.target.parentElement.classList[0] == 'right-side' 
+        || data.target.parentElement.classList[0] == 'portrait' 
         ) {
             //
     } else {
@@ -19,31 +21,4 @@ container.addEventListener('click', function(data) {
             state = 'outFocus'
         }, 50)
     }
-    if (state == 'outFocus') {
-        changeImage();
-    }
 })
-
-const image = document.querySelector('.container-image');
-const imageArray = ['image-one', 'image-two'];
-let currentImage = 0;
-let changingDia = false;
-
-function changeImage() {
-    if (changingDia == false) {
-        changingDia = true;
-        image.style.opacity = '0';
-        setTimeout(function() {
-            image.classList.remove(imageArray[currentImage]);
-            currentImage++;
-            if (currentImage == imageArray.length) {
-                currentImage = 0;
-            }
-            image.classList.add(imageArray[currentImage]);
-            image.style.opacity = '0.5';
-        }, 1000);
-        setTimeout(function() {
-            changingDia = false;
-        }, 2000);
-    }
-}
